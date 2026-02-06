468bd5a686ae172c075e71be873cd53b6e4b86c0
Adam Driver <148883472+akd03@users.noreply.github.com>
2026-02-06 16:33:09 +0000

    Added Data Script Placeholders

---
+++ Scripts/Thermofluids_Parameters_GS.m
@@ -0,0 +1,4 @@
+%%  Thermofluids_Parameters_GS.m
+%   Adam Driver 06/02/26
+%   
+%   This script holds all model parameters for the GS Simscape model. 
---
+++ Scripts/Thermofluids_Parameters_LV.m
@@ -0,0 +1,6 @@
+%%  Thermofluids_Parameters_LV.m
+%   Adam Driver 06/02/26
+%   
+%   This script holds all model parameters for the LV Simscape model. This
+%   is implemented separately to GS to enable easier integration with the
+%   MBSE model. 
