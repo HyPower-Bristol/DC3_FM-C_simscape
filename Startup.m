feecb8a62cdf910903b2f924917498300a029912
Adam Driver <adam.driver.2022@bristol.ac.uk>
2026-02-09 15:21:34 +0000

    Update Startup.m
    Added path management.

--- Startup.m
+++ Startup.m
@@ -3,3 +3,10 @@
 %   
 %   This script runs all files necessary to setup the model when opened. 
 
+%% Loading File Structure
+disp("Loading DC3 FMC Model")
+[currentPath, ~, ~] = fileparts(mfilename('fullpath'));
+cd(currentPath);
+addpath(genpath(currentPath));
+fprintf('Project Root set to: %s\n', pwd);
+
