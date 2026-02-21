beb641e884e2563fa631a0e0ded88b30eb86cb15
Adam Driver <adam.driver.2022@bristol.ac.uk>
2026-02-21 17:51:19 +0000

    Naming Conventions

--- Scripts/Fluid_Properties.m
+++ Scripts/Fluid_Properties.m
@@ -9,3 +9,6 @@
 load("EthanolTable.mat");
 load("N2GasTables.mat");
 load("N2OTables.mat");
+
+%%
+ATMOSPHERIC_PRESS_Pa = 101325;                                             % Atmospheric Pressure, Pascals
