8d116796debcdd00779bd0e6a7867e68290a8dd0
Adam Driver <adam.driver.2022@bristol.ac.uk>
2026-04-01 14:59:53 +0100

    Update Engine_Properties.m

--- Scripts/Engine_Properties.m
+++ Scripts/Engine_Properties.m
@@ -7,5 +7,6 @@
 %% Constants
 Engine_NozzleExitArea_m2 = 0;  
 Engine_NozzleThroatArea_m2 = 0; 
-ExhaustGamma_JpKgK = 1.18;          % Gemini Assumed Value
+Chamber_Volume_m3 = 0;               
+Exhaust_Gamma_JpKgK = 1.18;         % Gemini Assumed Value
 g0_mps2 = 9.80665;                  % Standard Value, for conversion of Isp
