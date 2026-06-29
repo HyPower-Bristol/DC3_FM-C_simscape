5498cba2e7b0c251038a5186fb35d30ba9217b49
Adam Driver <adam.driver.2022@bristol.ac.uk>
2026-06-29 14:20:52 +0100

    Engine Test Model Update
    Feed system flow with arbitrary engine mass flow working,

--- Scripts/Engine_Properties.m
+++ Scripts/Engine_Properties.m
@@ -5,8 +5,8 @@
 
 
 %% Constants
-Engine_NozzleExitArea_m2 = 0;  
-Engine_NozzleThroatArea_m2 = 0; 
+Engine_NozzleExitArea_m2 = 0.017;     % temp
+Engine_NozzleThroatArea_m2 = 0.0007;  % temporary guesstimate 
 Chamber_Volume_m3 = 0;               
 Exhaust_Gamma_JpKgK = 1.18;         % Gemini Assumed Value
 g0_mps2 = 9.80665;                  % Standard Value, for conversion of Isp
