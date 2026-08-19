fc380fe63658c3a4500bf07dd9bca6fc64ab0428
Adam Driver <adam.driver.2022@bristol.ac.uk>
2026-08-19 17:46:06 +0100

    Added Ereg params
    Adding nat's ereg params (note injector ones don't match assumed so update engine.)

--- Scripts/Thermofluids_Parameters_LV.m
+++ Scripts/Thermofluids_Parameters_LV.m
@@ -50,6 +50,8 @@ OX_TANK_inlet_h = NaN;              % Inlet height, m
 OX_TANK_volume = 0.0155186;         % Tank volume, m^3 <-derived value (done)
 OX_TANK_Initial_Mass_frac = 0; 
 dipstick_gauge_start = 0.15;        % Dipstick height inside Ox tank
+OX_INJECTOR_orifice_area = 80*1e-6; % injector orifice area (m^2) -> from EReg_Tank_Drain_N2O_Init.m
+OX_INJECTOR_discharge_coefficient = 0.7; % injector orifice discharge coefficient -> from EReg_Tank_Drain_N2O_Init.m
 
 %% Fuel (IPA) Tank Parameters
 
@@ -68,11 +70,41 @@ FUEL_TANK_length = 0.175;            % Tank length, m
 FUEL_TANK_inlet_h = NaN;             % Inlet height, m
 FUEL_TANK_volume = 0.00517287;       % Tank volume, m^3 <-derived value
 FUEL_TANK_Initial_Mass_kg = 1.375; 
+FUEL_INJECTOR_orifice_area = 60*1e-6; % injector orifice area (m^2) -> from EReg_Tank_Drain_Init.m
+FUEL_INJECTOR_discharge_coefficient = 0.7; % injector orifice discharge coefficient -> from EReg_Tank_Drain_Init.m
 
 
 %% Engine (Feed) Initial conditions
 Engine_Initial_Fuel_Pressure_bar = 3;
 Engine_Initial_Ox_Pressure_bar = 3;
 
+
+%%E-Reg, valve orifice and behaviour model #18
+% SOME OF THESE VALUES MAY NEED CHANGING
+% used U2127994_RP3_E_Regs.pdf and old e-reg tank drain/servo system
+EREG_command_frequency_Hz = 50;  % 50Hz control frequency of servo
+EREG_command_sample_time_s = 1/ EREG_command_frequency_Hz;
+
+EREG_gear_ratio = 1.7;  % ratio of servo angle to valve angle
+EREG_valve_angle_min_deg = 0;
+EREG_valve_angle_max_deg = 90;
+EREG_servo_angle_max_deg = EREG_gear_ratio * EREG_valve_angle_max_deg;
+EREG_servo_rate_degps = 180; % without pressure load
+EREG_valve_backlash_deg = 3;  % +- 0.5 deg
+EREG_valve_dead_angle_deg = 5; 
+
+% Non-linear orifice area of valves vs servo angle, servo signal from 0 to
+% 1 is non-linearly related to the valve open area
+EREG_valve_angle_breakpoints_deg = 0:1:90; % the 1 deg spacing (was personal choice) coarse vs fine
+EREG_effective_angle_deg = max(EREG_valve_angle_breakpoints_deg - EREG_valve_dead_angle_deg, 0);
+EREG_normalised_area_table = 1.1 * ( 5e-6 * EREG_effective_angle_deg.^3 ...
+    - 6e-4 * EREG_effective_angle_deg.^2 ...
+    + 0.0278 * EREG_effective_angle_deg);
+EREG_normalised_area_table = min(max(EREG_normalised_area_table, 0), 1);
+
+%discharge coefficient, old Cv = 4 has been correct for
+% e-reg report states kv = 13
+Kv_max = 13;
+Cv_max = Kv_max / 0.865;  % converting from the metric to US imperial?
 
 
